c337873803abf16dd5ea6b5318bdeebcb22eb8e0
winslause shioso <wenbusale383@gmail.com>
2024-03-04 21:54:59 +0300

    Fix email configuration and update index.html

--- mail.py
+++ mail.py
@@ -3,7 +3,7 @@ from flask_mail import Mail, Message
 
 app = Flask(__name__)
 
-app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = 'wenslause300@gmail.com'
 app.config['MAIL_PASSWORD'] = 'Shioso@2023'
@@ -12,14 +12,15 @@ app.config['MAIL_USER_SSL'] = True
 
 mail = Mail(app)
 
+
 @app.route("/")
 def index():
-    msg = Message('Hello', sender = 'wenslause300@gmail.com', recipients = ['wenbusale383@gmail.com'])
-    msg.body= "Hello wenslause, i tried to send youa message using flask localhost as a learning practice"
+    msg = Message('Hello', sender='wenslause300@gmail.com', recipients=['wenbusale383@gmail.com'])
+    msg.body = "Hello wenslause, i tried to send youa message using flask localhost as a learning practice"
     mail.send(msg)
     return 'Message sent successfully'
 
 
 if __name__ == '__main__':
-    #DEBUG is SET to TRUE. CHANGE FOR PROD
+    # DEBUG is SET to TRUE. CHANGE FOR PROD
     app.run(debug=True)
